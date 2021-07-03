92d03198c8eb380dd94963f68e507429c5769cd9
Abhi812163 <76207414+Abhi812163@users.noreply.github.com>
2021-07-03 19:42:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(400,250);
+
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_color="";
+}
+
+function draw(){
+    image(video,0,0,640,480);
+    tint(tint_color);
+}
+
+function apply_filter(){
+    tint_color=document.getElementById("color").value;
+}
+
+function take_snapshot(){
+    save('filtered_photo.png');
+}
